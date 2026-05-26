54b471d4390dd4a33f63bd4f2ba604b66fef1a85
ellsaw <elliotsandmail@gmail.com>
2026-05-26 14:34:57 +0200

    add sizes to archive

--- next-js/src/components/Archive/Archive.tsx
+++ next-js/src/components/Archive/Archive.tsx
@@ -19,14 +19,20 @@ export default async function Archive({ title, page, terms, author }: Props) {
     const dateTimeFormatter = await wpGetDateTimeFormatter();
 
     return (
-        <div className="my-8">
+        <div className="my-8 px-4">
             <h2 className="text-3xl mb-8">{title}</h2>
             <ul className="flex flex-col gap-8">
                 {postPage.posts.map((post) => (
-                    <li key={post.ID} className="h-48">
-                        <Link href={post.path || ''} className="group flex gap-6 h-full w-full">
+                    <li key={post.ID} className="sm:h-48">
+                        <Link href={post.path || ''} className="group flex flex-col sm:flex-row gap-2 sm:gap-6 h-full w-full">
                             <div className="shrink-0 h-full aspect-video rounded-xl overflow-hidden">
-                                {post.thumbnail && <WPAttachmentImage attachmentImage={post.thumbnail} className="w-full h-full object-cover"/>}
+                                {post.thumbnail &&
+                                    <WPAttachmentImage
+                                        attachmentImage={post.thumbnail}
+                                        className="w-full h-full object-cover"
+                                        sizes="(max-width: 640px) 100vw, 21.3rem"
+                                        />
+                                    }
                             </div>
                             <div>
                                 <time dateTime={post.postDate.toISOString()} className="opacity-75">{dateTimeFormatter.format(post.postDate)}</time>
