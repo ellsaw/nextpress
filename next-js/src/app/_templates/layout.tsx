8a04ea918a9d3a490de428880411fc9dc2d629fe
ellsaw <elliotsandmail@gmail.com>
2026-06-03 19:04:51 +0200

    add and style header

--- next-js/src/app/_templates/layout.tsx
+++ next-js/src/app/_templates/layout.tsx
@@ -1,11 +1,11 @@
-import Header from "./parts/header";
+import Header from "./parts/header/header";
 import Footer from "./parts/footer/footer";
 
 export async function LayoutTemplate({ children }: Readonly<{children: React.ReactNode;}>) {
     return (
         <>
         <Header/>
-        <main className="container mx-auto min-h-svh">
+        <main className="container mx-auto min-h-svh py-8">
             {children}
         </main>
         <Footer/>
