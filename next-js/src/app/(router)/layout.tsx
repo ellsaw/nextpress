4471dfe85bf7947435e994ac0351f775dfea4614
ellsaw <elliotsandmail@gmail.com>
2026-06-04 12:10:07 +0200

    add draft mode

--- next-js/src/app/(router)/layout.tsx
+++ next-js/src/app/(router)/layout.tsx
@@ -3,8 +3,11 @@ import "@/app/_css/globals.css";
 import { LayoutTemplate } from "../_templates/layout";
 import OptionLoader from "@/lib/nextpress/repository/option-loader/option-loader";
 import getLanguageAttriubtes from "@/lib/nextpress/services/metadata/get-language-attribute";
+import { draftMode } from "next/headers";
 
 export default async function RootLayout({ children }: Readonly<{children: React.ReactNode;}>) {
+    const draftModeEnabled = (await draftMode()).isEnabled;
+
     await OptionLoader.instance().preLoadOptions();
 
     const languageAttributes = await getLanguageAttriubtes();
